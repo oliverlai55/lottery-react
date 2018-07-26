8e32aec83ad03582a869baa845f755c4313a7fa1
Oliver Lai <oliverlai55@gmail.com>
2018-07-26 13:31:48 -0400

    Complete project

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ class App extends Component {
     manager: '',
     players: [],
     balance: '',
+    value: '',
+    message: ''
   };
 
   async componentDidMount() {
@@ -19,6 +21,33 @@ class App extends Component {
     this.setState({ manager, players, balance });
   }
 
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+    
+    this.setState({ message: 'Waiting on transaction success...' });
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether')
+    });
+
+    this.setState({ message: 'You have been entered!' });
+  };
+
+  onClick = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({ message: 'Waiting on transaction success...' });
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    });
+
+    this.setState({ message: 'A winner has been picked!' });
+  };
+
   render() {
     return (
       <div>
@@ -27,6 +56,28 @@ class App extends Component {
           This contract is managed by {this.state.manager}
           There are currently {this.state.players.length} people entered, competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!
         </p>
+        <hr />
+        
+        <form onSubmit={this.onSubmit}>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input
+              value={this.state.value}
+              onChange={event => this.setState({ value: event.target.value })}
+            />
+          </div>
+          <button>Enter</button>
+        </form>
+
+        <hr />
+
+        <h4>Ready to pick a winner?</h4>
+        <button onClick={this.onClick}>Pick a winner!</button>
+
+        <hr />
+
+        <h1>{this.state.message}</h1>
       </div>
     )
   }
